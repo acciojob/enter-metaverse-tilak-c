a95f3a61b3643b5e4a4333bce5da2d9742435ee9
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 11:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const pElement=document.getElementById("status");
+const buttonElement=document.getElementById("enterBtn");
+buttonElement.addEventListener("click",function(){
+	pElement.innerHTML="<h1>Entered Metaverse</h1>";
+})
